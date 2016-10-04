e90980cbd1a5778b88313097377a6665c4dbff9e
Olenka <olenka_zhdan@ukr.net>
2016-10-04 10:34:01 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
-var name = prompt('Input hello')
-var hello="Hello world!"
-document.writeln(hello);
-alert(hello);
-console.log(hello);
+/*function sum (a, b, c) {
+    var result = a + b + c;
+    return result;
+}
+
+var num1 = Number (prompt('Day of birth'));
+var num2 = Number (prompt('Month of birth'));
+var num3 = Number (prompt('Year of birth'));
+var result = sum(num1, num2, num3);
+document.writeln(result);
+
+function max (a, b) {
+         if (a > b) {
+    } else {
+        return b;
+    }
+}
+*/
+
+// Task01 - Write a code that returns any string value.
+// For example, next code return a boolean value: 
+// return false;
+
+/*function task01() {
+    return // TODO: Write your code here
+}*/
+
+var arr = ['one', 'teo', 'three'];
+arr[1] = 'ten';
+
+
+
+
